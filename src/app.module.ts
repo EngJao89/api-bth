a90f9fb80905a30f5652ca2a068325a182dcd00f
EngJao89 <eng_joao@outlook.com>
2024-12-11 17:41:26 -0300

    feat(bth20-modules-and-logs): implements logs, pipes and modules of auth users

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './lib/prisma.module';
+import { AuthModule } from './auth/auth.module';
 import { ThrottlerModule } from '@nestjs/throttler';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, PrismaModule, ThrottlerModule, ConfigModule],
+  imports: [
+    AuthModule,
+    UserModule,
+    PrismaModule,
+    ThrottlerModule,
+    ConfigModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
